29a9f61168eda8d06542f95933707ef9608fae07
TME520 <tme520@MacBookdeTME520.lan>
2019-05-14 23:00:48 +1000

    Experimenting with remote kubectl

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
 FROM ubuntu
 MAINTAINER tme520@gmail.com
-RUN apt update && apt upgrade -y && apt-get install -y apt-transport-https curl gnupg apt-utils && apt autoremove && apt autoclean
+RUN apt update && apt upgrade -y && apt-get install -y apt-transport-https curl gnupg apt-utils iputils-ping netcat && apt autoremove && apt autoclean
 RUN curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 RUN echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
 RUN apt update && apt-get install -y kubectl
 EXPOSE 80 443 22
 ADD appLifecycleMgr.sh /
+ADD config /
 ENTRYPOINT ["/appLifecycleMgr.sh"]
 CMD []
--- appLifecycleMgr.sh
+++ appLifecycleMgr.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-echo "DERPING "$@ >> /root/.minikube/tamere
+echo "DERPING "$@ > /root/.minikube/tamere
 echo "Arguments are "$@
 echo "Disks are"
 df -h
@@ -8,7 +8,13 @@ echo "Adding user deploy"
 useradd -d /deploy_home/ -m -s /bin/bash deploy
 id deploy >> /root/.minikube/tamere
 cat /root/.minikube/tamere
-mkdir /deploy_home/minikube_cnf/
-cp -rp /root/.minikube/* /deploy_home/minikube_cnf/
-ls -lisa /deploy_home/minikube_cnf/
+echo "Creating kube config"
+mkdir -p $HOME/.kube/
+mv /config $HOME/.kube/
+#echo "Listing pods"
+#kubectl config set-cluster test-cluster --server=http://kubernetes:8443
+#kubectl config use-context test-cluster
+#kubectl get pods
+ping -c 3 kubernetes
+nc -zvvvt kubernetes 8443
 exit 0
