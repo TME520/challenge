e46b2aa51928e38d7425945a4a83914e5a4a97ce
TME520 <tme520@ip-10-101-33-43.ap-southeast-2.compute.internal>
2019-05-15 11:03:32 +1000

    Finally got kubectl from container to talk to local cluster

--- appLifecycleMgr.sh
+++ appLifecycleMgr.sh
@@ -2,12 +2,12 @@
 
 echo "DERPING "$@ > /root/.minikube/tamere
 echo "Arguments are "$@
-echo "Disks are"
-df -h
+#echo "Disks are"
+#df -h
 #echo "Adding user deploy"
 #useradd -d /deploy_home/ -m -s /bin/bash deploy
 #id deploy >> /root/.minikube/tamere
-cat /root/.minikube/tamere
+#cat /root/.minikube/tamere
 echo "Copying minikube config"
 mkdir /minikube_copy
 cp -pr /root/.minikube/* /minikube_copy/
@@ -15,14 +15,13 @@ ls /minikube_copy/
 echo "Creating kube config"
 mkdir -p $HOME/.kube/
 mv /config $HOME/.kube/
-# sed -i -e 's/AAAAAAAA/kubernetes/g' $HOME/.kube/config
-sed -i -e 's/AAAAAAAA/192.168.99.101/g' $HOME/.kube/config
+sed -i -e 's/AAAAAAAA/kubernetes/g' $HOME/.kube/config
 cat $HOME/.kube/config
+#echo "Pinging kub"
+#ping -c 3 kubernetes
+#nc -zvvvt kubernetes 8443
+echo "Deployiong the app (init)"
+kubectl run hello-world --port=8080 --image=d0x2f/http-hello-world:v1.0.0 --generator=run-pod/v1
 echo "Listing pods"
-#kubectl config set-cluster test-cluster --server=http://kubernetes:8443
-#kubectl config use-context test-cluster
 kubectl get pods
-echo "Pinging kub"
-ping -c 3 kubernetes
-nc -zvvvt kubernetes 8443
 exit 0
